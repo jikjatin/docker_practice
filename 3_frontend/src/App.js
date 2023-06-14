d57d05e87ac65f17f67691b4e72649a4339bdb95
jikjatin <jatinrajput04@gmail.com>
2023-06-14 15:01:53 +0530

    name change

--- 3_frontend/src/App.js
+++ 3_frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          baby calm down
         </p>
         <a
           className="App-link"
